58e23942d937d8ecba6be501c202bfde278cff2e
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-11-11 00:28:52 +0200

    Add project files.

---
+++ Lab_06.2recur/Lab_06.2recur.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Min, const int Max, int i)
+{
+	a[i] = Min + rand() % (Max - Min + 1);
+	if (i < size - 1)
+		Create(a, size, Min, Max, i + 1);
+
+}
+void Vyvid(int* a, const int size, int i)
+{
+	cout << a[i] << " ";
+	if (i < size - 1)
+
+		Vyvid(a, size, i + 1);
+	else
+
+		return;
+}
+
+void Create1(int* a, const int size, int i)
+{
+	if (i <= size - 1)
+	{
+
+		if (i % 2 != 0)
+		{
+			cout << a[i] << " ";
+			Create1(a, size, i + 1);
+		}
+		else
+			Create1(a, size, i + 1);
+	}
+	else return;
+}
+
+int Sum(int* a, const int size, int i, int& suma)
+{
+
+	if (i % 2 != 0)
+		suma += a[i];
+	if (i < size-1)
+		return Sum(a, size, i + 1, suma);
+	else
+		return suma;
+
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 25;
+	int a[n];
+	int Min = -50;
+	int Max = 50;
+	int suma = 0;
+
+	Create(a, n, Min, Max, 0);
+	cout << endl;
+	Vyvid(a, n, 0);
+	cout << endl;
+	Create1(a, n, 0);
+	cout << endl;
+
+	
+	cout << "S = " << Sum(a, n, 0, suma) << endl;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06.2recur/Lab_06.2recur.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int suma = 0;
+			int a[] = { 2, 6, 8, 21, 9 };
+			int S = Sum(a, 5, 0, suma);
+
+			Assert::AreEqual(S, 27);
+		}
+	};
+}
